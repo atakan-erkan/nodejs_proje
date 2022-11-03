2475d0df3bd5e739c57ef3c013dbd0ed09733da8
Atakan Erkan <atakan.erkan@ogr.dpu.edu.tr>
2022-11-03 04:05:28 +0300

    lesson 2

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 import express from "express"
+import dotenv from "dotenv";
+import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
+
+dotenv.config();
+
+//connection to the DB
+conn(); 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT;
 
-app.get("/", (req,res)=>{
-    res.send('INDEX SAYFASI 4');
-});
+// ejs template engine
+app.set("view engine", 'ejs');
+
+// static files middleware
+app.use(express.static('public'));
+
+// routers
+app.use("/", pageRoute)
 
 app.listen(port, ()=>{
     console.log(`Application running on port: ${port}`);
